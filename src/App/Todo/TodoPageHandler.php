b6a21da4e30da17f49bc6f3069f4fe0aa573a505
SPashev <s.nurken92@gmail.com>
2020-12-03 12:43:05 +0600

    hot fix

--- src/App/Todo/TodoPageHandler.php
+++ src/App/Todo/TodoPageHandler.php
@@ -34,86 +34,86 @@ class TodoPageHandler implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        // $data = [
-        //     'forms' => [
-        //         'title' => [
-        //             'id' => 'title',
-        //             'label' => 'Title',
-        //             'type' => 'text', 
-        //             'placeholde' => 'Enter title...',
-        //             'class' => 'fomr-control'
-        //         ], 
-        //         'description' => [
-        //             'id' => 'description',
-        //             'label' => 'Description',
-        //             'type' => 'textarea', 
-        //             'placeholde' => 'Enter description...',
-        //             'class' => 'fomr-control'
-        //         ],
-        //         'status' => [
-        //             'btn' => [
-        //                 'id' => 'status',
-        //                 'label' => 'Status',
-        //                 'type' => 'checkbox',
-        //                 'checked' => True
-        //             ]
-        //         ],
-        //         'buttons' => [
-        //             'edit' => [
-        //                 'class' => 'btn btn-primary',
-        //                 'route' => 'todo.update',
-        //                 'icon' => 'fa fa-pencil-alt'
-        //             ],
-        //             'delete' => [
-        //                 'class' => "btn btn-danger",
-        //                 'route' => 'todo.delete',
-        //                 'icon' => "fa fa-times"
-        //             ]
-        //         ]
-        //     ],
-        //     'btn' => [
-        //         'add' => [
-        //             'class' => 'btn btn-success',
-        //             'route' => 'todo.save',
-        //             'type' => 'submit',
-        //             'icon' => 'fa fa-plus'
-        //         ]
-        //     ]
-        // ];
+        $data = [
+            'forms' => [
+                'title' => [
+                    'id' => 'title',
+                    'label' => 'Title',
+                    'type' => 'text', 
+                    'placeholde' => 'Enter title...',
+                    'class' => 'fomr-control'
+                ], 
+                'description' => [
+                    'id' => 'description',
+                    'label' => 'Description',
+                    'type' => 'textarea', 
+                    'placeholde' => 'Enter description...',
+                    'class' => 'fomr-control'
+                ],
+                'status' => [
+                    'btn' => [
+                        'id' => 'status',
+                        'label' => 'Status',
+                        'type' => 'checkbox',
+                        'checked' => True
+                    ]
+                ],
+                'buttons' => [
+                    'edit' => [
+                        'class' => 'btn btn-primary',
+                        'route' => 'todo.update',
+                        'icon' => 'fa fa-pencil-alt'
+                    ],
+                    'delete' => [
+                        'class' => "btn btn-danger",
+                        'route' => 'todo.delete',
+                        'icon' => "fa fa-times"
+                    ]
+                ]
+            ],
+            'btn' => [
+                'add' => [
+                    'class' => 'btn btn-success',
+                    'route' => 'todo.save',
+                    'type' => 'submit',
+                    'icon' => 'fa fa-plus'
+                ]
+            ]
+        ];
 
-        // $adapter = new Adapter([
-        //     'driver' => 'Pdo',
-        //     'dsn'    => 'mysql:dbname=mezzio;host=localhost;charset=utf8',
-        //     'username' => 'admin',
-        //     'password' => '1q2w3e4r'
-        // ]);
-        // $sql = new Sql($adapter);
+        $adapter = new Adapter([
+            'driver' => 'Pdo',
+            'dsn'    => 'mysql:dbname=mezzio;host=localhost;charset=utf8',
+            'username' => 'admin',
+            'password' => '1q2w3e4r'
+        ]);
+        $sql = new Sql($adapter);
 
-        // if($request->getMethod() == 'POST') {
+        if($request->getMethod() == 'POST') {
 
-        //     $data = $request->getParsedBody();
-        //     if(!isset($data['status'])) {
-        //         $data['status'] = 'off';
-        //     }
+            $data = $request->getParsedBody();
+            if(!isset($data['status'])) {
+                $data['status'] = 'off';
+            }
 
-        //     $insert = $sql->insert();
-        //     $insert->into('tasks');
-        //     $insert->columns(array_keys($data));
-        //     $insert->values(array_values($data));
-        //     $statement = $sql->prepareStatementForSqlObject($insert);
-        //     $statement->execute();
+            $insert = $sql->insert();
+            $insert->into('tasks');
+            $insert->columns(array_keys($data));
+            $insert->values(array_values($data));
+            $statement = $sql->prepareStatementForSqlObject($insert);
+            $statement->execute();
             
-        //     return new RedirectResponse('/todo');
-        // }
+            return new RedirectResponse('/todo');
+        }
 
-        // $select = $sql->select();
-        // $select->from('tasks');
+        $select = $sql->select();
+        $select->from('tasks');
 
-        // $statement = $sql->prepareStatementForSqlObject($select);
-        // $results = $statement->execute();
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
 
-        // $data['tasks'] = $results;
+        $data['tasks'] = $results;
 
-        // return new HtmlResponse($this->template->render('todo::index', $data));
+        return new HtmlResponse($this->template->render('todo::index', $data));
     }
 }
